5322ecde3f328b2cc5875b1b3bec95816aed0489
Sajib32 <Sajibnstu@gmail.com>
2015-04-03 12:25:29 +0600

    PDO and SMTP emails

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,11 @@
 class HomeController extends BaseController {
 
 	public function home()
-	{
+	{	
+		Mail::send('emails.auth.test', array('name' => 'Sajib'), function($message){
+			$message->to('sajibcste@gmail.com', 'Monirul Islam')->subject('Test email');
+		});
+
 		return View::make('home');
 	}
 
